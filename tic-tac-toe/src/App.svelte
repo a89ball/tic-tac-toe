aa69a543ca5b9b13b2efa627cad28e4b3da43a9a
Abdifatah <abdifatah.101@outlook.com>
2022-05-09 14:12:23 +0100

    Attempting to write if statement within html

--- tic-tac-toe/src/App.svelte
+++ tic-tac-toe/src/App.svelte
@@ -1,6 +1,43 @@
 <script>
 	export let playerOne;
 	export let playerTwo;
+	window.addEventListener("DOMContentLoaded", () => {
+		const tiles = Array.from(document.querySelectorAll(".tile"));
+		const playerDisplay = document.querySelector(".display-player");
+		const resetButton = document.querySelector("#reset");
+		const announcer = document.querySelector(".announcer");
+
+		let currentPlayer = "X";
+		let board = ["", "", "", "", "", "", "", "", ""];
+		let isGameRunning = true;
+
+		const player1Won = "PLAYER1_WON";
+		const player2Won = "PLAYER2_WON";
+		const draw = "DRAW";
+
+		const winningCombinations = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+
+
+		// If both conditions are true, current player will be given a turn and the game continues
+		const userAction = (tile, index) => {
+			if (isValidAction(tile) && isGameRunning) {
+				tile.innerText = currentPlayer;
+				tile.classList.add(`player${currentPlayer}`);
+				updateBoard(index);
+				handleResultValidation();
+				changePlayer();
+			}
+		};
+	});
 </script>
 
 <main>
@@ -11,22 +48,27 @@
 		<p>May the best man win. Good luck {playerOne} and {playerTwo}</p>
 	</section>
 	<section class="container">
-		<div class="tile"></div>
-		<div class="tile"></div>
-		<div class="tile"></div>
-		<div class="tile"></div>
-		<div class="tile"></div>
-		<div class="tile"></div>
-		<div class="tile"></div>
-		<div class="tile"></div>
-		<div class="tile"></div>
-	</section>
-<section class = "announcer hide">
+		<div class="tile" />
+		<div class="tile" />
+		<div class="tile" />
+		<div class="tile" />
+		<div class="tile" />
+		<div class="tile" />
+		<div class="tile" />
+		<div class="tile" />
+		<div class="tile" />
 
-</section>
-<section class="controls">
-	<button id ="reset">Reset</button>
-</section>
+		{#if roundWon 
+		announce(currentPlayer === 'X' ? PLAYER1_WON : PLAYER2_WON);
+		isGameActive = false;
+		return;
+		}
+	{/if}
+	</section>
+	<section class="announcer hide" />
+	<section class="controls">
+		<button id="reset">Reset</button>
+	</section>
 </main>
 
 <style>
@@ -44,6 +86,7 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
+
 
 	@media (min-width: 640px) {
 		main {
