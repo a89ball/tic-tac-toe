189d9bbe8774f9a9cbc8f6cc543e195c7a65a89c
Abdifatah <abdifatah.101@outlook.com>
2022-05-10 10:29:31 +0100

    created function which loops through win conditions

--- tic-tac-toe/src/App.svelte
+++ tic-tac-toe/src/App.svelte
@@ -26,41 +26,67 @@
 			[2, 4, 6],
 		];
 
+		// Function which which loops through win conditions
+
+		function handleResultValidation() {
+			let roundWon = false;
+			for (let i = 0; i <= 7; i++) {
+				const winCondition = winningConditions[i];
+				const a = board[winCondition[0]];
+				const b = board[winCondition[1]];
+				const c = board[winCondition[2]];
+				if (a === "" || b === "" || c === "") {
+					continue;
+				}
+				if (a === b && b === c) {
+					roundWon = true;
+					break;
+				}
+			}
+
+			if (roundWon) {
+				announce(currentPlayer === "X" ? PLAYER1_WON : PLAYER2_WON);
+				isGameActive = false;
+				return;
+			}
+
+			if (!board.includes("")) announce(TIE);
+		}
+
 		// Using a switch statement in order to announce the outcome of the game
 
 		const announce = (type) => {
-        switch(type){
-            case PLAYER2_WON:
-                announcer.innerHTML = 'Player <span class="player1">O</span> Won';
-                break;
-            case PLAYER1_WON:
-                announcer.innerHTML = 'Player <span class="player2">X</span> Won';
-                break;
-            case TIE:
-                announcer.innerText = 'Tie';
-        }
-        announcer.classList.remove('hide');
-
-		const changePlayer = () => {
-			playerDisplay.classList.remove(`player${currentPlayer}`);
-			currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-			playerDisplay.innerText = currentPlayer;
-			playerDisplay.classList.add(`player${currentPlayer}`); 
-		}
-		// If both conditions are true, current player will be given a turn and the game continues
-		const userAction = (tile, index) => {
-			if (isValidAction(tile) && isGameRunning) {
-				tile.innerText = currentPlayer;
-				tile.classList.add(`player${currentPlayer}`);
-				updateBoard(index);
-				handleResultValidation();
-				changePlayer();
+			switch (type) {
+				case PLAYER2_WON:
+					announcer.innerHTML =
+						'Player <span class="player1">O</span> Won';
+					break;
+				case PLAYER1_WON:
+					announcer.innerHTML =
+						'Player <span class="player2">X</span> Won';
+					break;
+				case TIE:
+					announcer.innerText = "Tie";
 			}
+			announcer.classList.remove("hide");
+
+			const changePlayer = () => {
+				playerDisplay.classList.remove(`player${currentPlayer}`);
+				currentPlayer = currentPlayer === "X" ? "O" : "X";
+				playerDisplay.innerText = currentPlayer;
+				playerDisplay.classList.add(`player${currentPlayer}`);
+			};
+			// If both conditions are true, current player will be given a turn and the game continues
+			const userAction = (tile, index) => {
+				if (isValidAction(tile) && isGameRunning) {
+					tile.innerText = currentPlayer;
+					tile.classList.add(`player${currentPlayer}`);
+					updateBoard(index);
+					handleResultValidation();
+					changePlayer();
+				}
+			};
 		};
-
-	}
-
-
 	});
 </script>
 
