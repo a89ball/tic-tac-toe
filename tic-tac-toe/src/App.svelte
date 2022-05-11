0f28533e441626b7e86776d1f485098e5eba1e04
Abdifatah <abdifatah.101@outlook.com>
2022-05-11 13:13:42 +0100

    Changed up format

--- tic-tac-toe/src/App.svelte
+++ tic-tac-toe/src/App.svelte
@@ -3,13 +3,27 @@
 	export let playerTwo;
 	window.addEventListener("DOMContentLoaded", () => {
 		const tiles = Array.from(document.querySelectorAll(".tile"));
-		const playerDisplay = document.querySelector(".display-player");
-		const resetButton = document.querySelector("#reset");
-		const announcer = document.querySelector(".announcer");
-
-		let currentPlayer = "X";
+		let gameActive = true;
 		let board = ["", "", "", "", "", "", "", "", ""];
-		let isGameRunning = true;
+		let currentPlayer = "X";
+
+
+
+
+		let user1 = {
+			player: document.getElementById('usr1').value,
+			
+		}; 
+		let user2= {
+			player: document.getElementById('usr2').value,
+	
+		}
+
+		let users = user1 + user2;
+
+
+	localStorage.setItem('UserList', JSON.stringify(users));
+
 
 		const player1Won = "PLAYER1_WON";
 		const player2Won = "PLAYER2_WON";
@@ -25,82 +39,61 @@
 			[0, 4, 8],
 			[2, 4, 6],
 		];
-
-		// Function which which loops through win conditions
-
-		function handleResultValidation() {
-			let roundWon = false;
-			for (let i = 0; i <= 7; i++) {
-				const winCondition = winningConditions[i];
-				const a = board[winCondition[0]];
-				const b = board[winCondition[1]];
-				const c = board[winCondition[2]];
-				if (a === "" || b === "" || c === "") {
-					continue;
+		function checkForWinner() {
+			winningCombinations.forEach(function (combination) {
+				let check = combination.every(
+					(index) => tiles[index].innerText.trim() == currentPlayer
+				);
+				if (check) {
+					highlightTiles(combination);
+					alert(currentPlayer + " has won");
 				}
-				if (a === b && b === c) {
-					roundWon = true;
-					break;
-				}
-			}
 
-			if (roundWon) {
-				announce(currentPlayer === "X" ? PLAYER1_WON : PLAYER2_WON);
-				isGameActive = false;
-				return;
-			}
+			
+			});
 
-			if (!board.includes("")) announce(TIE);
+
 		}
 
-		// Using a switch statement in order to announce the outcome of the game
+		function highlightTiles(combination) {
+			combination.forEach(function (index) {
+				tiles[index].classList.add("highlight");
+			});
+		}
+		tiles.forEach(function (tile) {
+			tile.addEventListener("click", function () {
+				if (tile.innerText.trim() != "") return;
+				tile.innerText = currentPlayer;
+				checkForWinner();
+				currentPlayer = currentPlayer == "X" ? "O" : "X";
+			});
+		});
 
-		const announce = (type) => {
-			switch (type) {
-				case PLAYER2_WON:
-					announcer.innerHTML =
-						'Player <span class="player1">O</span> Won';
-					break;
-				case PLAYER1_WON:
-					announcer.innerHTML =
-						'Player <span class="player2">X</span> Won';
-					break;
-				case TIE:
-					announcer.innerText = "Tie";
-			}
-			announcer.classList.remove("hide");
 
-			//If statement that checks if the action made by player is valid. if valid, it's falsey if invalid it's truthy
+		//let flag = false;
 
-			const isValidAction = (tile) => {
-				if (tile.innerText === "X" || tile.innerText === "O") {
-					return false;
-				}
+		//checkForWinner = () => {
+		//if (check) {
+		//highlightCells(combination);
+		//flag = true; // make the flag true, if player has won.
+		//}
+		//};
 
-				return true;
-			};
+		//tiles.forEach((tile) => {
+		//if (flag) {
+		//	return;
+		//if the player has won, then return..
+		//	else {
+		//	tile.innerHTML = currentPlayer;
 
-			const updateBoard = (index) => {
-				board[index] = currentPlayer;
-			};
+		//	checkForWinner(); // before that create winning combinations..
 
-			const changePlayer = () => {
-				playerDisplay.classList.remove(`player${currentPlayer}`);
-				currentPlayer = currentPlayer === "X" ? "O" : "X";
-				playerDisplay.innerText = currentPlayer;
-				playerDisplay.classList.add(`player${currentPlayer}`);
-			};
-			// If both conditions are true, current player will be given a turn and the game continues
-			const userAction = (tile, index) => {
-				if (isValidAction(tile) && isGameRunning) {
-					tile.innerText = currentPlayer;
-					tile.classList.add(`player${currentPlayer}`);
-					updateBoard(index);
-					handleResultValidation();
-					changePlayer();
-				}
-			};
-		};
+		//	currentPlayer =
+		//	currentPlayer == "X"
+		//? "O"
+		//: "X";
+		//	}
+		//});
 	});
 </script>
 
@@ -108,6 +101,20 @@
 	<section>
 		<h1>Tic Tac Toe</h1>
 		<p>Please enter your names</p>
+
+		<div class="form-group">
+			<label for="usr">Player One Name:</label>
+			<input type="text" class="form-control" id="usr1" />
+			<br /><br />
+			<button onclick="store()">Add Player</button>
+		</div>
+
+		<div class="form-group">
+			<label for="usr">Player Two Name:</label>
+			<input type="text" class="form-control" id="usr2" />
+			<br /><br />
+			<button onclick="store()">Add Player</button>
+		</div>
 
 		<p>May the best man win. Good luck {playerOne} and {playerTwo}</p>
 	</section>
@@ -124,7 +131,19 @@
 	</section>
 	<section class="announcer hide" />
 	<section class="controls">
-		<button id="reset">Reset</button>
+		<button
+			id="reset"
+			onclick="document.getElementByClass(
+		 'tile').value = ''">Reset</button
+		>
+	</section>
+	<section>
+		<div class="scoreboard">
+			<div class="socreboard__name">{playerOne}</div>
+			<div class="socreboard__name">{playerTwo}</div>
+			<div class="socreboard__score">0</div>
+			<div class="socreboard__score">0</div>
+		</div>
 	</section>
 </main>
 
